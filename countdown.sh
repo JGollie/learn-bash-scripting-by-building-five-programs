ad7a3a81edc0c9a20a59948eca1a3d9d25ff4c06
JGollie <jonathan.lgolden@gmail.com>
2024-03-23 17:05:27 -0400

    Save progress: 740.1

--- countdown.sh
+++ countdown.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Program that counts down to zero from a given argument
 echo -e "\n~~ Countdown Timer ~~\n"
-if [[
-     $1 -gt 0 ]]
+if [[ $1 -gt 0 ]]
 then
 : ' 
     for (( i = $1; i >= 0; i-- ))
@@ -12,6 +11,10 @@ then
     done
 '
 I=$1 
+    while [[ $I -ge 0 ]]
+    do
+        echo $I
+    done
 else
     echo Include a positive integer as the first argument.
 fi 
