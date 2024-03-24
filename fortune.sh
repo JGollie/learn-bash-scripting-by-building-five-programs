82599a700be772e0c034d9e0a16d6c7800cb55aa
JGollie <jonathan.lgolden@gmail.com>
2024-03-24 03:48:13 -0400

    formatting

--- fortune.sh
+++ fortune.sh
@@ -4,7 +4,7 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
 
-GET_FORTUNE() {
+function GET_FORTUNE() {
    echo Ask a yes or no question: 
    read QUESTION
 }
