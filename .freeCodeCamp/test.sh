5ce8614c9372b7bbf8f0fa256a7b6145f5fb7daa
Jonathan Golden <jonathan.lgolden@gmail.com>
2024-03-25 00:05:39 -0500

    Add files via upload

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-echo -e "\n~~ Questionnaire ~~\n"
-QUESTION1="What's your name?";
-echo $QUESTION1
-read NAME;
-echo Hello $NAME.;
-QUESTION2="Where are you from?";
-echo $QUESTION2
-read LOCATION;
-echo Hello $NAME from $LOCATION.;
-QUESTION3="What's your favorite coding website?"
-echo $QUESTION3
-read WEBSITE;
+# Program to run my other four programs
 
+./questionnaire.sh
+./countdown.sh a
+./bingo.sh
+./fortune.sh
 
-echo $WEBSITE
